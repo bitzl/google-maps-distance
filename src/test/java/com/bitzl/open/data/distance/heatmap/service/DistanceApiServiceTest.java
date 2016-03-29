7e6893137599f3f5e2aab9e1ece1a586b990bed1
Marcus Bitzl <marcus@bitzl.com>
2016-03-29 18:37:12 +0200

    Added Mockito.

---
+++ src/test/java/com/bitzl/open/data/distance/heatmap/service/DistanceApiServiceTest.java
@@ -0,0 +1,32 @@
+package com.bitzl.open.data.distance.heatmap.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by marcu on 29.03.2016.
+ */
+public class DistanceApiServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void query() throws Exception {
+
+    }
+
+    @Test
+    public void encode() throws Exception {
+
+    }
+
+    @Test
+    public void encode1() throws Exception {
+
+    }
+}
