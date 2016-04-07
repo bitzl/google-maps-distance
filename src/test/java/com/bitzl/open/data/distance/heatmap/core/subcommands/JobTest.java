800a140c55a7aa822fde166e79577442ea925e68
Marcus Bitzl <marcus@bitzl.com>
2016-04-07 23:54:31 +0200

    Added OpenPojo and tests.

---
+++ src/test/java/com/bitzl/open/data/distance/heatmap/core/subcommands/JobTest.java
@@ -0,0 +1,23 @@
+package com.bitzl.open.data.distance.heatmap.core.subcommands;
+
+
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.test.impl.GetterTester;
+import com.openpojo.validation.test.impl.SetterTester;
+import org.junit.Test;
+
+import static com.openpojo.reflection.impl.PojoClassFactory.getPojoClass;
+
+public class JobTest {
+
+    @Test
+    public void testGetterAndSetter() {
+        Validator validator = ValidatorBuilder.create()
+                .with(new GetterTester())
+                .with(new SetterTester())
+                .build();
+        validator.validate(getPojoClass(Job.class));
+    }
+
+}
