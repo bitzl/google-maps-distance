d876d05ca5feabac5f5992e813b71aca1e49a87a
Marcus Bitzl <marcus@bitzl.com>
2016-04-08 22:20:48 +0200

    Added tests.

--- src/test/java/com/bitzl/open/data/distance/heatmap/gather/service/DistanceApiServiceTest.java
+++ src/test/java/com/bitzl/open/data/distance/heatmap/gather/service/DistanceApiServiceTest.java
@@ -1,24 +1,28 @@
 package com.bitzl.open.data.distance.heatmap.gather.service;
 
-import com.bitzl.open.data.distance.heatmap.gather.apis.GoogleDistanceApi;
 import com.bitzl.open.data.distance.heatmap.core.model.location.Coordinate;
+import com.bitzl.open.data.distance.heatmap.gather.apis.GoogleDistanceApi;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class DistanceApiServiceTest {
+
+    private GoogleDistanceApi googleDistanceApi;
 
     private DistanceApiService distanceApiService;
 
     @Before
     public void setUp() throws Exception {
-        GoogleDistanceApi googleDistanceApi = mock(GoogleDistanceApi.class);
+        googleDistanceApi = mock(GoogleDistanceApi.class);
         distanceApiService = new DistanceApiService(googleDistanceApi);
     }
 
@@ -29,11 +33,29 @@ public class DistanceApiServiceTest {
 
     @Test
     public void encodeShouldEncodeMultipleCoordinates() throws Exception {
-        List<Coordinate> coordinates = Arrays.asList(
+        List<Coordinate> coordinates = asList(
                 new Coordinate(1.23, 2.34),
                 new Coordinate(3.33, 4.44),
                 new Coordinate(7.77, 5.55)
         );
         assertThat(distanceApiService.encode(coordinates), is("1.23,2.34|3.33,4.44|7.77,5.55"));
+    }
+
+    @Test
+    public void apiShouldBeCalledWithApiKey() {
+        distanceApiService.query("API_KEY", asList(new Coordinate(1.1, 1.2), new Coordinate(2.1, 2.2)), new Coordinate(2.1, 2.2));
+        verify(googleDistanceApi).query(matches("API_KEY"), any(), any());
+    }
+
+    @Test
+    public void apiShouldBeCalledWithOriginCoordinates() {
+        distanceApiService.query("API_KEY", asList(new Coordinate(1.1, 1.2), new Coordinate(2.1, 2.2)), new Coordinate(2.1, 2.2));
+        verify(googleDistanceApi).query(any(), eq("1.1,1.2|2.1,2.2"), any());
+    }
+
+    @Test
+    public void apiShouldBeCalledWithDestinationCoordinates() {
+        distanceApiService.query("API_KEY", asList(new Coordinate(1.1, 1.2), new Coordinate(2.1, 2.2)), new Coordinate(2.1, 2.2));
+        verify(googleDistanceApi).query(any(), any(String.class), eq("2.1,2.2"));
     }
 }
